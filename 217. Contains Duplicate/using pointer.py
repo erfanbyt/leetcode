5f19f9edce1b2244a9875999ae3d799a4a70a74a
erfanbyt <89116723+erfanbyt@users.noreply.github.com>
2022-12-17 23:09:03 -0800

    Update using pointer.py

--- 217. Contains Duplicate/using pointer.py
+++ 217. Contains Duplicate/using pointer.py
@@ -1,3 +1,7 @@
+# using 2 pointers on a sorted list
+# time complexity: O(nlog n) --> come from the sorting algo.
+# space complexity: O(1) --> it is done in-place
+
 def containsDuplicate(self, nums: List[int]) -> bool:
 
     nums = sorted(nums)
@@ -5,7 +9,7 @@ def containsDuplicate(self, nums: List[int]) -> bool:
     right_pointer = 1
     left_pointer = 0
 
-    if len(nums) == 1:
+    if len(nums) == 1:  # to deal with the edge case of only one element
         return False
 
     while right_pointer <= len(nums) - 1:

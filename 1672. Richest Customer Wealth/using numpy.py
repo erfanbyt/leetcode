1446fd6f2a55e6e4fee18ebcaf49de259c350c6e
erfanbyt <89116723+erfanbyt@users.noreply.github.com>
2022-12-19 23:46:53 -0800

    Update using numpy.py

--- 1672. Richest Customer Wealth/using numpy.py
+++ 1672. Richest Customer Wealth/using numpy.py
@@ -1,3 +1,5 @@
+# this would be the most efficient solution is numpy was already installed, but on leetcode server it was not fast enough!
+
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
 

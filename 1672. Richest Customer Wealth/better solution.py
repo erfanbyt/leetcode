1917d31e6212d1026baf3a991aa1b7531a4ab158
erfanbyt <89116723+erfanbyt@users.noreply.github.com>
2022-12-19 23:43:49 -0800

    Create better solution.py

---
+++ 1672. Richest Customer Wealth/better solution.py
@@ -0,0 +1,11 @@
+# time complexity: O(n^2) --> for iteration over each row and the summation on each row
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+
+        sum_accounts_person = []
+        for row in accounts:
+            sum_accounts_person.append(sum(row))
+
+        return max(sum_accounts_person)
+            

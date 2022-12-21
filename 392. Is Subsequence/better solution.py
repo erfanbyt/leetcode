6373e86e9ccf130534c6c1a689cdf5b98bad313a
erfanbyt <89116723+erfanbyt@users.noreply.github.com>
2022-12-21 11:29:57 -0800

    Create better solution.py

---
+++ 392. Is Subsequence/better solution.py
@@ -0,0 +1,20 @@
+# run time: O(n)
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        pointer_s = 0
+        pointer_t = 0
+
+        while pointer_t < len(t) and pointer_s < len(s):
+            if s[pointer_s] == t[pointer_t]:
+                pointer_s += 1
+            pointer_t += 1
+
+        if pointer_s == len(s):
+            return True
+        
+
+        return False
+
+        

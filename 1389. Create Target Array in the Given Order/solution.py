e722d23f3192a20f23776c5a3b37f7ab6142f6c7
erfanbyt <89116723+erfanbyt@users.noreply.github.com>
2022-12-21 23:23:52 -0800

    Create solution.py

---
+++ 1389. Create Target Array in the Given Order/solution.py
@@ -0,0 +1,10 @@
+# run time: O(n)
+
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+
+        target = []
+        for i, num in zip(index, nums):
+            target.insert(i, num)
+
+        return target

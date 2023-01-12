2623df508a6bcc4ab7e12460ff0760f3b094d8f4
erfanbyt <89116723+erfanbyt@users.noreply.github.com>
2023-01-12 13:46:55 -0800

    Create solution.py

---
+++ 66. Plus One/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+
+        str_num = [str(i) for i in digits]
+        num = str(int("".join(str_num)) + 1)
+        list_num = [int(s) for s in num]
+        # print(list_num)
+
+        return list_num
